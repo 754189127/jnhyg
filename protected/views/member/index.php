ff86d5beab4aeb0d825fab8826967c01ea6f0aee
754189127 <754189127@qq.com>
2014-08-11 00:34:41 +0800

    会员首页

--- protected/views/member/index.php
+++ protected/views/member/index.php
@@ -6,96 +6,88 @@
 $this->pageTitle=Yii::app()->name . ' - Login';
 ?>
 <div class="searchBar">
-	<form>
-	<table>
-    	<tr>
-        	<td>期数：<select id="periodicalId"></select></td>
-            <td>厂商编号：<input id="companyCode1"/>-<input  id="companyCode2"/></td>
-            <td>地址：<input id="address" /></td>
-            <td>邮箱：<input  id="email"/></td>
-            <td><input  type="button" value="搜索"  id="searchBt" /> <input  type="reset" value="重置"/></td>
-        </tr>
-    </table>
+    <form>
+        <table>
+            <tr>
+                <td>期数：<select id="periodicalId"></select></td>
+                <td>厂商编号：<input id="companyCode1"/>-<input  id="companyCode2"/></td>
+                <td>地址：<input id="address" /></td>
+                <td>邮箱：<input  id="email"/></td>
+                <td><input  type="button" value="搜索"  id="searchBt" /> <input  type="reset" value="重置"/></td>
+            </tr>
+        </table>
     </form>
 </div>
 
 <div class="form">
-    		<div  class="panel-default" id="panel-title"><span>厂商管理列表</span></div>
-            <div class="table-list" id="table-list">
-            	 
-           </div>
-           <div class="buttonBar">
-           		<input class="btn-button" id="addCompany"  type="button" value="A添加"  /> 
-                <input class="btn-button"  type="button" value="导入"  /> 
-                <input class="btn-button"  type="button" value="打印"  />
-                <input class="btn-button"  type="button" value="复制"  />  
-           </div>
+    <div  class="panel-default" id="panel-title"><span>厂商管理列表</span></div>
+    <div class="table-list" id="table-list">
+
+    </div>
+    <div class="buttonBar">
+        <input class="btn-button" id="addCompany"  type="button" value="A添加"  />
+        <input class="btn-button"  type="button" value="导入"  />
+        <input class="btn-button"  type="button" value="打印"  />
+        <input class="btn-button"  type="button" value="复制"  />
+    </div>
 </div>
 <script>
 
- $(document).ready(function(){  
-      $(".flexme2").tableSorter({headers: {0:{ sorter: false}}});  
- })  
-		
-$('.flexme2').flexigrid({
-	height : 'auto',
-	striped : false
-});
-ajaxGetperiodical(0);
-ajaxsLoadList();
-function ajaxsLoadList(){
-	var periodicalId = $("#periodicalId").val(),
-		companyCode1 = $("#companyCode1").val(),
-		companyCode2 = $("#companyCode2").val(),
-		address = $("#address").val(),
-		email = $("#email").val();
-	if(periodicalId==0){
-		alert('请选择期数');return false;
-	}	
-	$.ajax({
-		url:'<?php echo $this->createUrl("company/search")?>',
-		type:'post',
-		data:{'periodicalId':periodicalId,'companyCode1':companyCode1,'companyCode2':companyCode2,'address':address,'email':email},
-		success:function(data){
-			$("#table-list").html(data);
-		}
-	});	
-}
+    $(document).ready(function(){
+        $(".flexme2").tableSorter({headers: {0:{ sorter: false}}});
+    })
+
+    $('.flexme2').flexigrid({
+        height : 'auto',
+        striped : false
+    });
+    ajaxGetperiodical(0);
+    ajaxsLoadList();
+    function ajaxsLoadList(){
+        var periodicalId = $("#periodicalId").val(),
+            companyCode1 = $("#companyCode1").val(),
+            companyCode2 = $("#companyCode2").val(),
+            address = $("#address").val(),
+            email = $("#email").val();
+        if(periodicalId==0){
+            alert('请选择期数');return false;
+        }
+        $.ajax({
+            url:'<?php echo $this->createUrl("company/search")?>',
+            type:'post',
+            data:{'periodicalId':periodicalId,'companyCode1':companyCode1,'companyCode2':companyCode2,'address':address,'email':email},
+            success:function(data){
+                $("#table-list").html(data);
+            }
+        });
+    }
 
 
-//搜索
-$("#searchBt").click(function(){
-	ajaxsLoadList();
-});
+    //搜索
+    $("#searchBt").click(function(){
+        ajaxsLoadList();
+    });
 
-//添加 
-$("#addCompany").click(function(){
-	$.ajax({
-		url:'<?php echo $this->createUrl('/company/add')?>',
-		success:function(result){
-			art.dialog({
-			title: '添加厂商!'
-			content: result,
-			 button: [
-			{
-				name: '保存',
-				callback: function () {
-					this.content('你同意了').time(2);
-					return false;
-				},
-				focus: true
-			},
-		   
-			{
-				name: '关闭'
-			}
-		] 
-			
-		
-		});	
-		}
-	});
-	
-});
+    //添加
+    $("#addCompany").click(function(){
+        art.dialog({
+            content: '如果定义了回调函数才会出现相应的按钮',
+            button: [
+                {
+                    name: '同意',
+                    callback: function () {
+                        this.content('你同意了').time(2);
+                        return false;
+                    },
+                    focus: true
+                },
+
+                {
+                    name: '关闭'
+                }
+            ]
+
+        });
+    });
 
 </script>
